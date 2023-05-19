e275c872e8a4395bafd275eb7775f54ad2eead6f
Pavel Tarasov <pavel.a.tarasov@gmail.com>
2023-05-19 23:48:36 +0300

    main example updated

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from intern import InternsList
+from manager import SchedulerManager
 from schedule import MonthlySchedule
 from scheduler import GreedyScheduler
 
 if __name__ == "__main__":
     interns_list = InternsList.from_csv("interns.csv")
     monthly_schedule = MonthlySchedule(year=2023, month=5, first_er_shift_day=2)
-
-    scheduler = GreedyScheduler(interns=interns_list, schedule=monthly_schedule)
-    scheduler.generate_schedule()
-    print("---")
-    print(scheduler.schedule)
-    print("---")
-    print(scheduler.calculate_score())
+    manager = SchedulerManager(
+        interns=interns_list,
+        schedule=monthly_schedule,
+        scheduler_classes=[GreedyScheduler],
+        num_runs=10,
+    )
+    manager.run_schedulers()
+    best_result = manager.get_best_result()
+    print(best_result['score'])
+    print(best_result['schedule'])
